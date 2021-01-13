a49b275985af518ea6e344359a6963e66aaa26b2
mackenziemcclellan <mccl6596@stthomas.edu>
2021-01-13 15:15:46 -0600

    Escape

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import DegreePlan from './DegreePlan';
 import {BrowserRouter, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 function App() {
+  function school() {
+    var schoolID = document.getElementById("schoolList");
+  }
+  function major() {
+    var majorID = document.getElementById("majorList");
+  }
   return (
     <div className="App">
       <img class="LogoClass" src= "Logo.PNG"> </img>
