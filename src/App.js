7d1c1379605d304b58f884fe7071d9eef2aed363
tinatrinh1012 <nhungoctrinhle@gmail.com>
2021-01-13 15:28:25 -0600

    blah

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import DegreePlan from './DegreePlan';
 import {BrowserRouter, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './LogoClass.css'
+
+
 function App() {
   function school() {
     var schoolID = document.getElementById("schoolList");
@@ -14,7 +17,7 @@ function App() {
   } 
   return (
     <div className="App">
-      <img class="LogoClass" src= "Logo.PNG"> </img>
+      <img className="LogoClass" src="Logo.JPG"/>
       <h1>School</h1>
       <select id="schoolList" onchange="school()">
         <option>University of St. Thomas</option>
