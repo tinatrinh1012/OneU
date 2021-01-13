3d0f0df017b17dc9c7ac441540f76beabf815930
tinatrinh1012 <nhungoctrinhle@gmail.com>
2021-01-12 20:34:49 -0600

    create button and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import DegreePlan from './DegreePlan';
+import {BrowserRouter, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      OneU app
-      hh
+      <h1>This is OneU app</h1>
+
+      <BrowserRouter>
+      
+        <Link to='/degreeplan'>
+          <button className='btn btn-primary'>Create a degree plan</button>
+        </Link>
+
+        <Route path='/degreeplan' component={DegreePlan} exact/>
+
+        
+      
+      </BrowserRouter>
+
+      
+
+      
     </div>
   );
 }
