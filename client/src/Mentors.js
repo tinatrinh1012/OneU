3e44e6d5927ce34fd87e41bef63104a02c43ee1c
mackenziemcclellan <mccl6596@stthomas.edu>
2021-01-22 14:50:09 -0600

    work!

--- client/src/Mentors.js
+++ client/src/Mentors.js
@@ -9,7 +9,9 @@ function Mentors() {
             <div className="Box">
                 <div className = "Kyle">
                     <img className= "Profile" src="img_avatar.png" alt="Avatar"/>
-                    <div className="KyleContent">
+                    <h15>Kyle Andrews</h15>
+                </div>
+                <div className="Content">
                         <h11>Kyle Andrews- Sales Engineer @ Jamf</h11>
                         <br/>
                         <h12>2016 University of St. Thomas Graduate</h12>
@@ -17,7 +19,6 @@ function Mentors() {
                         <h13>Majors: Entrepreneurship & Econonmics, Minor: Biology</h13>
                         <br/>
                         <h14>#software #edtech #medicaldevice #startups</h14>
-                    </div>
                 </div>
             </div>
             <div className="Box">
